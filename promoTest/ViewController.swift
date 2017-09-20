76a29f104c9099287ccb36771f66678058a5aab7
Dmitriy <denikaev.apps@gmail.com>
2017-09-20 09:22:35 +0300

    Initial Commit

---
+++ promoTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  promoTest
+//
+//  Created by Дмитрий Деникаев on 20.09.17.
+//  Copyright © 2017 Dmitriy Denikaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
